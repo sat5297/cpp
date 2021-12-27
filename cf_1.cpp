20e1a0da3b99c984c7e59658265894ce7df2efc1
shakti <shaktiratan.5297@gmail.com>
2021-12-27 20:40:58 +0530

    create branch

--- cf_1.cpp
+++ cf_1.cpp
@@ -4,20 +4,22 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
-	int t;
+	int t,z;
 	cin >> t;
 	for(int i = 0; i < t; i++)
 	{
-		int a,b;
-		cin >> a >> b;
-		
-		int diff = abs(a-b);
-		
-		if(diff % 2 == 0)
-			cout << diff/2 << endl;
-		else
-			cout << diff/2 + 2 << endl;	
+	    int a,b,c;
+	    cin >> a >> b >> c;
+	    if((a + b == c) || (a + c == b) || (b + c == a))
+	        cout << "YES\n";
+	    else if((a % 2 == 0)&&(b == c))
+	        cout << "YES\n";
+	    else if((b % 2 == 0) && (a == c))
+	        cout << "YES\n";
+	    else if((c % 2 == 0) && (a == b))
+	        cout << "YES\n";
+	    else
+	        cout << "NO\n";   
 	}
 	return 0;
 }
